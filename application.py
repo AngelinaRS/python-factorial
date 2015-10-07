06dafd87e874c63a76583fb1e0856ed0b2502a40
AngelinaRS <candy26y_@hotmail.com>
2015-10-07 11:15:40 -0600

    factorial function

--- application.py
+++ application.py
@@ -1,3 +1,23 @@
-# Aqui escribe tu codigo
+"Factorial"
 
-print "Hello World"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+def reset():
+    """This cleans the screen"""
+    if os.name == ("posix"): #In Linux
+        os.system("clear")
+    elif os.name == ("ce", "nt", "dos"): #In windows
+        os.system("cls")
+reset()
+
+def factorial(number):
+    """This creates the factorial of the any number"""
+    if number <= 1: #If the number is lesser than "0" will return "1"
+        return 1
+    #If number is longer than "1" will elaborate the factorial of any number
+    return number * factorial(number-1)
+
